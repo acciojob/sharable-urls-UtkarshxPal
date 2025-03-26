e57890ace8b3c58931097424f6fba548ece33696
UtkarshxPal <150327255+UtkarshxPal@users.noreply.github.com>
2025-03-27 01:22:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// your code here
+const form = document.getElementById("form") ; 
+
+
+form.addEventListener("submit" , (e)=>{
+	e.preventDefault();
+	const name = document.getElementById("name").value ; 
+const year = document.getElementById("year").value;
+let baseUrl = " https://localhost:8080/"
+	
+const updatedUrl = `${baseUrl}?${name ? `name=${name}` : ""}${name && year ? "&" : ""}${year ? `year=${year}` : ""}`;
+
+
+	const h3 = document.getElementById("url") ; 
+	h3.textContent = updatedUrl ; 
+	
+})
